93e54b322a52d888719b8abbdefefc6403959987
nikita351 <nikitamironov351@gmail.com>
2021-11-11 13:58:27 +0200

    add role

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,27 @@
 provider "aws" {
-
-   region     = "us-east-1"  
+   region = var.region  
 }
 
 resource "aws_instance" "prod_server" {
-
     ami                    = "ami-083654bd07b5da81d"  
-    instance_type          = "t2.micro" 
-    key_name               = "id_rsa"
+    instance_type          = var.instance_type
+    key_name               = var.key_name
     count = 2
     vpc_security_group_ids = [aws_security_group.main.id]
 
      tags = {
       Name  = element(var.instance_tags, count.index)
-      Batch = "5AM"
   }
-
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "mini"
-      private_key = file("/Users/mini/.ssh/id_rsa")
-   }
 }
 
 resource "aws_security_group" "main" {
-
   dynamic "ingress" {
     for_each = ["22", "8080"]
     content {
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = [ "0.0.0.0/0", ]
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = [ "0.0.0.0/0", ]
     }
   }
 
@@ -45,8 +34,6 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_key_pair" "ssh_key" {
-
-  key_name   = "id_rsa"
+  key_name   = var.key_name
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6qzXkfNBXDWMM3pyjJOTPpry7jEO0Kg4axZJBxY3WhpJH7/O43HuPPjwwCCI8DvBiSuPh8vtx96qTxr9lIb//nTPFDogAImPDbZIgmpgSi5KQKk/dS/6ttnEuF+kTsBtuuUbG2kZbBAR8we937USCJqJ2RXuJlq8Ek8PJhsNSU4FA+f0mnHQx2njzFsOBOwZZ7nW9WLb5cMyD9QMpfHgUqdwX3CYZs4EV6Q41eXfRAEaXYiWJEIlx61Di9vaIoi/a8fRpMpsXdOdWHsj28cm5r2cC5kXUqST//QyUHR7gmxpLDYLmF5mZLdp45PZW9xhycQRBp3zH1bjYUC2FbzK4VwAyv0MKj5U+nmuja12yOuFnnhivbGdz6r7POeKYmpEnxpcL0A7CkQA0tpkKOZPLRXE0bsLAHpcp1K2L06PrhYFqwvVhVJflTcm0bPj7C/wlyiK7gh3YJvCrUwI7/Iebwf6Tho3Dm2zYBuvoDU5D04vDvjiqzZFetJt6DLw7qXE= mini@MacBook-Air-mini.local"
-
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,19 @@
 variable "instance_tags" {
-  type = list
-  default = ["PROD_SERVER", "TEST_SERVER"]
+    type = list
+    default = ["PROD_SERVER", "TEST_SERVER"]
+}
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "key_name" {
+    type = string
+    default = "id_rsa"
 }
