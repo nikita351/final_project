4d350bbc685200d5e3c004b33599971a3ff52eac
Mykyta_Mironov <Mykyta_Mironov@epam.com>
2021-11-15 12:43:36 +0200

    update tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,31 +6,12 @@ resource "aws_instance" "prod_server" {
     ami                    = "ami-083654bd07b5da81d"  
     instance_type          = var.instance_type
     key_name               = var.key_name
-    count = 2
+    count                  = 2
     vpc_security_group_ids = [aws_security_group.main.id]
 
      tags = {
       Name  = element(var.instance_tags, count.index)
   }
-}
-
-resource "aws_security_group" "main" {
-  dynamic "ingress" {
-    for_each = ["22", "8080"]
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = [ "0.0.0.0/0", ]
-    }
-  }
-
-  egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = [ "0.0.0.0/0", ]
-    }
 }
 
 resource "aws_key_pair" "ssh_key" {
---
+++ terraform/security_groups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "main" {
+  dynamic "ingress" {
+    for_each = ["22", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = [ "0.0.0.0/0", ]
+    }
+  }
+
+  egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = [ "0.0.0.0/0", ]
+    }
+}
